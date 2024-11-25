f48b1af8f612444c74a2fddbb63d94a186ea27ed
sheet25 <sheet25@proton.me>
2024-11-25 12:46:50 -0600

    Update streamlit_bBoard_app.py

--- streamlit_bBoard_app.py
+++ streamlit_bBoard_app.py
@@ -21,4 +21,4 @@ data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+data_load_state.text("Done! (using st.cache_data)")
