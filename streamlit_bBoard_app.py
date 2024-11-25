164d31d4e2a9fa3172872788dedb4fd4b5914ef2
sheet25 <sheet25@proton.me>
2024-11-25 10:39:05 -0600

    Update streamlit_bBoard_app.py

--- streamlit_bBoard_app.py
+++ streamlit_bBoard_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
