a9932da4827faea6b90b328bbe3998a9d90913a2
sheet25 <sheet25@proton.me>
2024-11-25 12:43:08 -0600

    Update streamlit_bBoard_app.py

--- streamlit_bBoard_app.py
+++ streamlit_bBoard_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-if "counter" not in st.session_state:
-    st.session_state.counter = 0
-
-st.session_state.counter += 1
-
-st.header(f"This page has run {st.session_state.counter} times.")
-st.button("Run it again")
+st.title('bBoard')
