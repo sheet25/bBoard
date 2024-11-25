d97bebf35722152d21049e660e321eac98d53c2d
sheet25 <sheet25@proton.me>
2024-11-25 12:21:25 -0600

    Update streamlit_bBoard_app.py

--- streamlit_bBoard_app.py
+++ streamlit_bBoard_app.py
@@ -1,16 +1,9 @@
 import streamlit as st
-import time
 
-'Starting a long computation...'
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
 
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
+st.session_state.counter += 1
 
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-
-'...and now we\'re done!'
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
