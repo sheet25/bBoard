ee9d52b0f28b0be98847ff351a790a1f00fffcfe
sheet25 <sheet25@proton.me>
2024-11-25 10:34:57 -0600

    Update streamlit_bBoard_app.py

--- streamlit_bBoard_app.py
+++ streamlit_bBoard_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
-import pandas as pd
+import numpy as np
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-df
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
